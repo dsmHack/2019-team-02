17f4940f4f97245a60f2d0c194ef79dae7988087
Andreas <achristianson@sourceallies.com>
2019-02-22 11:44:47 -0600

    blanking repo as we change direction.

--- scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-if [ "$TRAVIS_BRANCH" = "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
-
-  gcloud beta functions deploy helloWorld --runtime nodejs8 --trigger-http --source dist/index.js
-
-fi
--- test/example.spec.js
+++
@@ -1,5 +0,0 @@
-describe('example test', ()=>{
-    it('should pass',()=>{
-        expect(1).toBe(1);
-    })
-})
